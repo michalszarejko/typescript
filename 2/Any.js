1a0fc9341c0f5db4947f12d2c4ce41447b182ee0
Michal Szarejko <michalszarejko@INV-0250.local>
2025-01-23 10:45:23 +0100

    test

---
+++ 2/Any.js
@@ -0,0 +1,11 @@
+"use strict";
+{
+    let xx;
+    xx = "Michal";
+    xx = 9;
+    xx = {};
+    xx = () => { };
+    let xage = xx;
+    xx.age;
+    xx();
+}
---
+++ 2/Any.ts
@@ -0,0 +1,12 @@
+{
+    let xx : any;
+    xx = "Michal";
+    xx = 9;
+    xx = {};
+    xx = () => {};
+
+    let xage : number = xx;
+
+    xx.age;
+    xx();
+}
